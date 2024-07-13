98962ffe6289969a642f0ca89721947e531507f4
Stephen Badger <stephen.badger@gmail.com>
2024-07-13 19:29:21 +0100

    Connect to Redis

--- packages/warden-national-railway/submit-order/submit-order.js
+++ packages/warden-national-railway/submit-order/submit-order.js
@@ -1,5 +1,14 @@
 var redis = require("redis");
 
 export function main(event) {
-    var client = redis.createClient();
+    var client = redis.createClient(
+        {
+            username: process.env.REDIS_USER,
+            password: process.env.REDIS_PASSWORD,
+            socket: {
+                host: process.env.REDIS_HOST,
+                port: process.env.REDIS_PORT
+            }
+        }
+    );
 };
